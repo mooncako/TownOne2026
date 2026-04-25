a549ca8ecb7a536557cd34b2b5ccae60872009e5
liam000001206 <92518491+liam000001206@users.noreply.github.com>
2026-04-25 16:56:32 -0700

    Add RespawnZone
    Add Respawn Prefab
    Add Respawn Script
    Update PinBall

---
+++ Assets/Scripts/Gameplay/BallRespawn.cs
@@ -0,0 +1,29 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class BallRespawn : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Vector3 respawnLocation = new Vector3(0,0.5f,0);
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.TryGetComponent<PinBall>(out PinBall ball))
+        {
+            other.gameObject.transform.position = respawnLocation;
+            Vector3 RandomDir = Vector3.ProjectOnPlane(Random.onUnitSphere, Vector3.up).normalized;
+            ball.AddImpulse(RandomDir * 20.0f);
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
