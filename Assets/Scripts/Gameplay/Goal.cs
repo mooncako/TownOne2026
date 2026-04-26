706f92119e8513a3a9616cb2ce93fbbaf45f2d5d
matty-li <madelineli.van@gmail.com>
2026-04-26 14:51:40 -0700

    Score works! + For the right player

--- Assets/Scripts/Gameplay/Goal.cs
+++ Assets/Scripts/Gameplay/Goal.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
-    [SerializeField] private PlayerId goalOwnerID;
+    private PlayerInfo goalOwner;
+    private PlayerInfo goalScorer => GameStateManager.Instance.GetOpponentInfo(goalOwner);
+    [SerializeField] private Faction faction;
     [SerializeField] private int amt;
+
 
     void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent<PinBall>(out var pinBall))
         {
-            PlayerInfo goalOwner = GameStateManager.Instance.GetPlayerInfoFromID(goalOwnerID);
-            PlayerInfo goalScorer = GameStateManager.Instance.GetOpponentInfo(goalOwner);
+            if (faction == Faction.Heaven)
+                goalOwner = GameStateManager.Instance.HeavenPlayerInfo;
+            else if (faction == Faction.Hell)
+                goalOwner = GameStateManager.Instance.HellPlayerInfo;
 
             goalOwner.Score.UpdateScore(-amt);
             goalScorer.Score.UpdateScore(amt);
